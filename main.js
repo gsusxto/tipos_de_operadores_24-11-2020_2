c638e0163c78a33649a81d1cc73fb7c91b00b1a1
unknown <=>
2020-11-24 20:42:57 -0500

    tipos_operadores2

---
+++ main.js
@@ -0,0 +1,59 @@
+//operador de incremento
+// let numero1=5;
+// let numero2=15;
+// numero1++; //incrementar de 1 en 1
+// numero1 +=2; //incrementar por una cantidad deseada
+
+// console.log("contador es: ", numero1);
+
+//operador de decremento
+// let numero1=5;
+// let numero2=15;
+// numero1--; //decremento de 1 en 1
+// numero1 -=2; //deremento por una cantidad deseada
+
+// console.log("decremento es: ", numero1);
+
+//operador de exponencial
+// let numero1=5;
+// let numero2=15;
+// numero1**=numero2;  
+// console.log("exponencial: ", numero1);
+
+// operaciones de modulo numero par e impar
+// let numero1=13;
+// let numero2=2;
+
+// numero1 %=numero2;
+
+// if(numero1==0){
+//     console.log("el numero es par");
+// }
+// if(numero1==1){
+//     console.log("el numero es inpar");
+// }
+
+//operaciones matematicas simples
+let numero1=16;
+let numero2=2;
+let suma;
+let resta;
+let multi;
+let divi;
+let expo;
+let modulo;
+let raiz;
+suma = numero1 + numero2;
+console.log("suma: ", suma);
+resta = numero1 - numero2;
+console.log("resta: ", resta);
+multi = numero1 * numero2;
+console.log("multiplicacion: ", multi);
+divi = numero1 / numero2;
+console.log("division: ", divi);
+expo = numero1 ** numero2;
+console.log("exponente: ", expo);
+modulo = numero1 % numero2;
+console.log("Modulo: ", modulo);
+raiz = Math.sqrt(numero1);
+console.log("raiz cuadrada: ", raiz);
